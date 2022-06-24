162d00654cda30c57f7b5b55e3665576ece229fe
beerioh <Am_ok@hotmail.com>
2022-06-24 21:02:44 +0300

    modified:   ../EX02/index.js 	modified:   ../EX04/index.js 	modified:   ../EX05/index.js 	modified:   ../EX06/index.js 	modified:   ../EX07/index.html 	modified:   ../EX07/index.js 	../EX01/.vscode/ 	../EX04/index.html 	../EX06/index2.html 	../../fs-01-fibonacci-beerioh/

--- D01/EX03/index.js
+++ D01/EX03/index.js
@@ -1,17 +1,32 @@
-function compareNumbers(fnumber , snumber) {
-    if (fnumber > snumber)
+
+  /* if (fnumber > snumber)
+ {
+  console.log(`The number ${fnumber} is bigger than ${snumber}`) 
+ }
+ else if (fnumber === snumber)
+ {
+   console.log(`The number ${fnumber} is equal to ${snumber}`)
+ }
+ else if (fnumber < snumber)
+ {
+   console.log(`The number ${fnumber} is less than ${snumber}`)
+ }
+}
+This function should accept two parameters instead of one, where the first one is the number to be compared and the second one the one to compare with. You need to compare the numbers, and according to the right situation print in the console: 'The number X is bigger than Y' or 'The number X is less than Y' or 'The number X is equal to Y'. To test this exercise run: npm run testex3
+*/
+function compareNumbers(verA, verB) {
+  if (verA > verB)
   {
-   console.log(`The number ${fnumber} is bigger than ${snumber}`) 
+    console.log(`The number ${verA} is bigger than ${verB}`);
   }
-  else if (fnumber === snumber)
+  else if (verA === verB)
   {
-    console.log(`The number ${fnumber} is equal to ${snumber}`)
+    console.log(`The number ${verA} is equal to ${verB}`);
   }
-  else if (fnumber < snumber)
+  else if (verA < verB)
   {
-    console.log(`The number ${fnumber} is less than ${snumber}`)
+    console.log(`The number ${verA} is less than ${verB}`);
   }
 }
-
 // Do not remove or change this line, or the tests won't work
 export { compareNumbers };
